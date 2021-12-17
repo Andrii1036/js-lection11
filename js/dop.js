ffdff477afdf6a61607a078298d4b377e5a6a2a9
Андрій Ковалик <kovalic1036@gmail.com>
2021-12-17 21:24:56 +0200

    fixed function

--- js/dop.js
+++ js/dop.js
@@ -36,38 +36,40 @@
 // const inputJSON2 = '[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"},{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"},{"childs":[{"age":55,"firstName":"Kira"},{"age":19,"firstName":"Olha"}]}],"age":44,"firstName":"Michael"}]';
 
 
-const inputJSON = '[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"},{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"}],"age":44,"firstName":"Michael"}]';
+const inputJSON = '[[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"}],{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"}],"age":44,"firstName":"Michael"}]';
 const inputJSON2 = '[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"},{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"},{"childs":[{"age":55,"firstName":"Kira"},{"age":19,"firstName":"Olha"}]}],"age":44,"firstName":"Michael"}]';
 const inputJSON3 = '[{"childs":[{"age":25,"firstName":"Marta","childs":[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"},{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"},{"childs":[{"age":55,"firstName":"Kira"},{"age":19,"firstName":"Olha"}]}],"age":44,"firstName":"Michael"}]},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"},{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"}],"age":44,"firstName":"Michael"}]';
+const inputJSON4 = '[[[[{"childs":[{"age":25,"firstName":"Marta"},{"childs":[{"age":10,"firstName":"Arnold"},{"age":12,"firstName":"Nick"}],"age":32,"firstName":"Peter"}],"age":60,"firstName":"Alina"}]]],{"age":35,"firstName":"Barbara"},{"age":43,"firstName":"Jack"},{"age":52,"firstName":"Tom"},{"childs":[{"age":15,"firstName":"Mike"},{"age":16,"firstName":"Joshua"},{"childs":[{"age":55,"firstName":"Kira"},{"age":19,"firstName":"Olha"}]}],"age":44,"firstName":"Michael"}]';
 
-let parseArray = (array) => {
+let parseArray = (array, result) => {
     let parsedArray = JSON.parse(array);
-    let resultArray = [];
+    let resultArray = result || [];
 
-    let findObject = (WhereLook) => {
-        for (let item of WhereLook) {
-            if (Array.isArray(item)) {
-                findObject(item);
-            } else {
-                let temporaryObject = {};
-                for (let key in item) {
-                    if (typeof item[key] === 'object') {
-                        findObject(item[key]);
-                    } else {
-                        temporaryObject[key] = item[key];
-                    };
+    for (let item of parsedArray) {
+        if (Array.isArray(item)) {
+            parseArray(JSON.stringify(item), resultArray);
+        } else {
+            let temporaryObject = {};
+            let isTemporaryObjectOk = false
+            for (let key in item) {
+                if (typeof item[key] !== 'object') {
+                    isTemporaryObjectOk = true
+                    temporaryObject[key] = item[key];
+                } else {
+                    isTemporaryObjectOk = false
+                    parseArray(JSON.stringify(item[key]), resultArray)
                 };
+            };
+            if (isTemporaryObjectOk) {
                 resultArray.push(temporaryObject)
             };
         };
     };
 
-    findObject(parsedArray);
-    resultArray.sort((a, b) => a.firstName > b.firstName)
-    console.log(resultArray);
+    return resultArray.sort((a, b) => a.firstName > b.firstName ? 1 : a.firstName === b.firstName ? 0 : -1);
 };
 
-parseArray(inputJSON);
-parseArray(inputJSON2);
-parseArray(inputJSON3);
-console.log(JSON.parse(inputJSON2))
+console.log(parseArray(inputJSON));
+console.log(parseArray(inputJSON2));
+console.log(parseArray(inputJSON3));
+console.log(parseArray(inputJSON4));
